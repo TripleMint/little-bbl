73be88b381afa7965ae4bd8e61e24de2241280eb
Chris DiLorenzo <dilorenzo.christopher@gmail.com>
2014-01-16 18:25:12 -0500

    test types returned

--- test_littlebbl.py
+++ test_littlebbl.py
@@ -1,13 +1,14 @@
 # Tests for little bbl
 import littlebbl
+import six
 # import pytest
 
 
 def test_unicode_returned():
     k = littlebbl.resolve('310', 'EAST 46TH STREET', '7U', 'MH')
-    assert isinstance(k[0], unicode)
-    assert isinstance(k[1], unicode)
-    assert isinstance(k[2], unicode)
+    assert isinstance(k[0], six.text_type)
+    assert isinstance(k[1], six.text_type)
+    assert isinstance(k[2], six.text_type)
 
 
 def test_boroughs():
