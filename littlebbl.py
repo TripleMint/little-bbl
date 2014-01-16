01b8b5df74d2aff0c48a7470e62a3c65c4d530bd
Chris DiLorenzo <dilorenzo.christopher@gmail.com>
2014-01-16 18:50:09 -0500

    comments and header

--- littlebbl.py
+++ littlebbl.py
@@ -1,18 +1,41 @@
-# HEADER
-# Explain the borough naming
-# License
-# CREDitsz
-# expand the boroguh resolver?
+'''
+url: https://suitey.github.io/little-bbl
 
-# scrape to identify error type, ie bad addy vs bad unit id
+The MIT License (MIT)
+
+Copyright (c) 2014 Suitey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+# For making HTTP requests
 import requests
+
+# For inspecting the response DOM
 from bs4 import BeautifulSoup
+
+# For supporting python 2 and 3
 import six
 
 
 def get_borough_number(borough_name):
     '''
-    return the appropriate borough number, or None
+    return the appropriate borough number or None
     '''
     return {
         'MH': '1',
@@ -23,15 +46,12 @@ def get_borough_number(borough_name):
         }.get(borough_name, None)
 
 
-# Your friend.
-# Inputs: listing object
-# Returns:
-#   3-tuple of strings, (borough, block, lot)
-#   or returns 0 if failure
-
 def resolve(street_num, street_name, apt_num, borough_name):
     '''
     All parameters should be strings
+    Returns:
+        3-tuple of strings, (borough, block, lot)
+        Returns None if failure
     '''
 
     URL = 'http://webapps.nyc.gov:8084/CICS/fin1/find001i'
@@ -42,6 +62,11 @@ def resolve(street_num, street_name, apt_num, borough_name):
 
     borough = get_borough_number(borough_name)
 
+    # Break if borough not in MH, BX, BK, QN, SI
+    if not borough:
+        print 'Invalid Borough... breaking.'
+        return
+
     # Generate data to post
     POST_FORM = {
         'FAPTNUM': apt_num,
@@ -51,9 +76,6 @@ def resolve(street_num, street_name, apt_num, borough_name):
         'FSTNAME': street_name
     }
 
-    # Fields should be None. An empty string should not be called
-    # an error (eg. an empty string for FAPTNUM is expected for coops)
-
     # Python 2 and 3 compatible
     values = list(POST_FORM.values()) if six.PY3 else POST_FORM.values()
 
@@ -62,30 +84,38 @@ def resolve(street_num, street_name, apt_num, borough_name):
               'to make POST request')
         return None
 
+    # Get the page
     response = requests.post(URL, data=POST_FORM)
 
-    # Try the POST request, and make some soup
+    # Make sure got solid response
     if response.status_code == 200:
+
+        # Make some soup
         soup = BeautifulSoup(response.text)
     else:
         print('Request Error: Did not get 200 HTTP response code')
         return None
 
+    # Find lot and block in soup
     lot_tag = soup.find('input', attrs={'name': 'q49_lot'})
     block_tag = soup.find('input', attrs={'name': 'q49_block_id'})
 
+    # Make sure we found block and lot
     if lot_tag and block_tag:
         lot = lot_tag.get('value')
         block = block_tag.get('value')
 
     else:
+
+        # If we did not get a lot and block we find what the error was
+        # in the soup instead
         error = soup.find(
             'form', attrs={'name': 'FINDMP1A'}).find(
             'font', attrs={'color': 'red'}).text
         if error:
-            print("ERROR: {}".format(error))
+            print('ERROR: {}'.format(error))
         else:
-            print("Unknown Error")
+            print('Unknown Error')
         return None
 
     # Block and lot are already in unicode
